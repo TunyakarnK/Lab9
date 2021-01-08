655ca90a8e47e47c9e9632f650e0aa3a1dbd3c3f
TunyakarnK <tunyakarn2544@gmail.com>
2021-01-08 16:39:19 +0700

    lab9

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,22 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int count;
+    float sum,dev;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        dev += pow(atof(textline.c_str()),2);
+        count++;
+    }
+    cout << "Number of data = "<< count;
+    cout << setprecision(3);
+    cout << "\nMean = "<< sum/count;
+    cout << "\nStandard deviation = "<< pow(((dev/count)-pow(sum/count,2)),0.5);
+}
